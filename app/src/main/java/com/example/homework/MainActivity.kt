f327c429f208b7677c8e8f716f0bbc72c521261e
lenochka69 <93248482+lenochka69@users.noreply.github.com>
2021-11-14 15:16:33 +0300

    commit2

--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Это поросенок удачи", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
@@ -45,7 +45,7 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.setting -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
